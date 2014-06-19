3e824d7d190aa322767581b107d5ee0d81b6309c
okamuuu <okamuuu@gmail.com>
2014-06-19 09:16:43 +0900

    added model.

--- backbone/coffeescript-meet-backbonejs/app/scripts/main.js
+++ backbone/coffeescript-meet-backbonejs/app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+var obj = new Backbone.Model();
+ 
+obj.set({name: "Murata"});
+obj.set({age: 20});
+ 
+console.log("obj: " + JSON.stringify(obj));
+console.log("obj.name: " + obj.get("name"));
