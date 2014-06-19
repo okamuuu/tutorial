26e76200e05c77e41c10c7c09b3cecb8139e6b80
okamuuu <okamuuu@gmail.com>
2014-06-19 09:43:35 +0900

    implemented Hello Famo.us

--- famous/app/src/main.js
+++ famous/app/src/main.js
@@ -1,29 +1,22 @@
-/* globals define */
 define(function(require, exports, module) {
-    'use strict';
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+  'use strict';
+  var Engine = require('famous/core/Engine');
+  var Surface = require('famous/core/Surface');
 
-    // create the main context
-    var mainContext = Engine.createContext();
+  var mainContext = Engine.createContext();
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
+  function createSurface() {
+    var firstSurface = new Surface({
+      size: [200, 100],
+      content: 'Hello Famo.us',
+      properties: {
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: '#FA5C4F'
+      }
     });
-
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
-
-    mainContext.add(centerSpinModifier).add(logo);
+    mainContext.add(firstSurface);
+  }
+ 
+  createSurface();
 });
