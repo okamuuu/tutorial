2a85595baa5453788a64eaaf99988631bb83bb47
okamuuu <okamuuu@gmail.com>
2014-06-20 20:27:37 +0900

    fnidished Lesson1:Displaying Content

--- famous/app/src/main.js
+++ famous/app/src/main.js
@@ -5,18 +5,16 @@ define(function(require, exports, module) {
 
   var mainContext = Engine.createContext();
 
-  function createSurface() {
-    var firstSurface = new Surface({
-      size: [200, 100],
-      content: 'Hello Famo.us',
-      properties: {
-        color: 'white',
-        textAlign: 'center',
-        backgroundColor: '#FA5C4F'
-      }
-    });
-    mainContext.add(firstSurface);
-  }
- 
-  createSurface();
+  var firstSurface = new Surface({
+    size: [200, 400],
+    content: 'hello world',
+    properties: {
+      color: 'white',
+      textAlign: 'center',
+      backgroundColor: '#FA5C4F'
+    }
+  });
+
+
+  mainContext.add(firstSurface);
 });
