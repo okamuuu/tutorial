601cca85ae44a9d0193a9a6880bae028eea6e62b
okamuuu <okamuuu@gmail.com>
2014-06-21 12:11:12 +0900

    http://famo.us/university/famous-101/animating/2/

--- famous/app/src/main.js
+++ famous/app/src/main.js
@@ -8,59 +8,22 @@ define(function(require, exports, module) {
 
   var mainContext = Engine.createContext();
 
-  var aligin = [0.5, 0.5];
-  var origin = [0.5, 0.5];
-
-  mainContext.add(modifier).add(surface);
-
-  var view = new View();
-
-  view.add(new Surface({
+  var surface = new Surface({
+    size: [100, 100],
     properties: {
+      color: 'white',
+      textAlign: 'center',
       backgroundColor: '#FA5C4F'
     }
-  }));
-
-  var viewModifier = new StateModifier({
-    size: [200, 200],
-    origin: origin,
-    aligin: aligin
   });
 
-  var positions = [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]
-  ];
+  var stateModifier = new StateModifier();
 
-  for (var i = 0; i < positions.length; i++) {
-    var surface = new Surface({
-      size: [true, true],
-      content: 'origin:<br />' + positions[i]
-    });
+  mainContext.add(stateModifier).add(surface);
 
-    var modifier = new StateModifier({
-      origin: positions[i],
-      align: positions[i]
-    });
+  stateModifier.setTransform(
+    Transform.translate(100, 300, 0),
+    { duration: 1000, curve: 'easeInOut' }
+  );
 
-    view.add(modifier).add(surface);
-  }
-
-  mainContext.add(viewModifier).add(view);
-
-  for (var j = 0; j < positions.length; j++) {
-    var _surface = new Surface({
-      size: [true, true],
-      content: 'align:<br>' + positions[j]
-    });
-    
-    var _modifier = new StateModifier({
-      origin: positions[j],
-      align: positions[j]
-    });
-    
-    mainContext.add(_modifier).add(_surface);
-  }
 });
