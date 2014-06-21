c770522f9ed4196429c35308a6bc29403774f944
okamuuu <okamuuu@gmail.com>
2014-06-21 11:06:07 +0900

    finished http://famo.us/university/famous-101/positioning/4/

--- famous/app/src/main.js
+++ famous/app/src/main.js
@@ -2,19 +2,81 @@ define(function(require, exports, module) {
   'use strict';
   var Engine = require('famous/core/Engine');
   var Surface = require('famous/core/Surface');
+  var Transform = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
 
   var mainContext = Engine.createContext();
 
-  var firstSurface = new Surface({
-    size: [200, 400],
-    content: 'hello world',
-    properties: {
-      color: 'white',
-      textAlign: 'center',
-      backgroundColor: '#FA5C4F'
-    }
+/*
+  function createSurface() {
+    var surface = new Surface({
+      size: [100, 100],
+      content: 'surface',
+      properties: {
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: '#FA5C4F'
+      }
+    });
+
+    mainContext.add(surface);
+  }
+
+  function createModifiedSurface() {
+    var modifiedSurface = new Surface({
+      size: [100, 100],
+      content: 'modified surface',
+      properties: {
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: '#FA5C4F'
+      }
+    });
+
+    var stateModifier = new StateModifier({
+      transform: Transform.translate(150, 100, 0)
+    });
+
+    mainContext.add(stateModifier).add(modifiedSurface);
+  }
+*/
+
+  var translateModifierOne = new StateModifier({
+    transform: Transform.translate(200, 0, 0)
   });
 
+  var translateModifierTwo = new StateModifier({
+    transform: Transform.translate(200, 0, 0)
+  });
 
-  mainContext.add(firstSurface);
+  var rotateModifierOne = new StateModifier({
+    transform: Transform.rotateZ(Math.PI/4)
+  });
+
+  var rotateModifierTwo = new StateModifier({
+    transform: Transform.rotateZ(Math.PI/4)
+  });
+
+  var redSurface = new Surface({
+    size: [100, 100],
+    classes: ['red-bg']
+  });
+
+  var greySurface = new Surface({
+    size: [100, 100],
+    classes: ['grey-bg']
+  });
+
+  mainContext
+    .add(translateModifierOne)
+    .add(rotateModifierOne)
+    .add(redSurface);
+
+  mainContext
+    .add(translateModifierTwo)
+    .add(rotateModifierTwo)
+    .add(greySurface);
+
+  //createSurface();
+  //createModifiedSurface();
 });
