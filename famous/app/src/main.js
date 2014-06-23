8113991883ea9cd7298d40deac6a93eccb1b28ec
okamuuu <okamuuu@gmail.com>
2014-06-23 09:37:55 +0900

    finished http://famo.us/university/famous-101/eventing/2/

--- famous/app/src/main.js
+++ famous/app/src/main.js
@@ -2,17 +2,12 @@ define(function(require, exports, module) {
   'use strict';
   var Engine = require('famous/core/Engine');
   var Surface = require('famous/core/Surface');
-  var Transform = require('famous/core/Transform');
-  var StateModifier = require('famous/modifiers/StateModifier');
-  var Easing = require('famous/transitions/Easing');
-  var Transitionable = require('famous/transitions/Transitionable');
-  var SpringTransition = require('famous/transitions/SpringTransition');
-  Transitionable.registerMethod('spring', SpringTransition);
 
   var mainContext = Engine.createContext();
 
   var surface = new Surface({
-    size: [100, 100],
+    size: [undefined, 100],
+    content: 'click me',
     properties: {
       color: 'white',
       textAlign: 'center',
@@ -20,15 +15,11 @@ define(function(require, exports, module) {
     }
   });
 
-  var stateModifier = new StateModifier({
-    origin: [0.5, 0]
+  mainContext.add(surface);
+
+  surface.on('click', function() {
+    surface.setProperties({
+      backgroundColor: '#878785'
+    });
   });
-
-  mainContext.add(stateModifier).add(surface);
-
-  stateModifier.setTransform(
-    Transform.translate(0, 300, 0),
-    { method: 'spring', period: 1000, dampingRatio: 0.3 }
-  );
-
 });
