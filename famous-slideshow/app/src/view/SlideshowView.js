1074c2288e13616a0f613ebe60e59d17ea2dad82
okamuuu <okamuuu@gmail.com>
2014-06-23 21:18:11 +0900

    Create and Add SlideshowView

---
+++ famous-slideshow/app/src/view/SlideshowView.js
@@ -0,0 +1,27 @@
+/*** EmptyView ***/
+
+// define this module in Require.JS
+define(function(require, exports, module) {
+
+    // Import additional modules to be used in this view 
+    var View = require('famous/core/View');
+    var Surface = require('famous/core/Surface');
+    var Transform = require('famous/core/Transform');
+    var StateModifier = require('famous/modifiers/StateModifier');
+
+    var SlideView = require('views/SlideView');
+
+    function SlideshowView() {
+        View.apply(this, arguments);
+        var slideView = new SlideView();
+        this.add(slideView);
+    }
+
+    SlideshowView.prototype = Object.create(View.prototype);
+    SlideshowView.prototype.constructor = SlideshowView;
+
+    SlideshowView.DEFAULT_OPTIONS = {};
+
+    module.exports = SlideshowView;
+});
+
