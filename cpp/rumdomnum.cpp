6e3bd215f4baa8c8cdeefac92d32a7df67ef0a23
okamuuu <okamuuu@gmail.com>
2014-07-28 09:37:08 +0900

    added ctime.

--- cpp/rumdomnum.cpp
+++ cpp/rumdomnum.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
+srand((unsigned)time(NULL));
   cout << rand() << endl;
   cout << rand() << endl;
   cout << rand() << endl;
